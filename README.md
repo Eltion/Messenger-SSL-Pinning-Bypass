652e9c2404140590b32f7b5e92c67de047a7023c
elti <eltimusa4@gmail.com>
2022-12-01 16:15:44 +0100

    Init

---
+++ README.md
@@ -0,0 +1,67 @@
+# Messenger SSL Pinning Bypass
+
+Bypass Messenger SSL pinning on Android devices.  
+Supported ABIs: `arm64-v8a`  
+Latest version: `v388.0.0.23.106`
+
+If you like this project:  
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/eltimusa4q)
+
+## Patched APK (No Root)
+
+Download the latest patched APK: 
++ [messenger-v388.0.0.23.106.apk](https://github.com/Eltion/Messenger-SSL-Pinning-Bypass/releases/download/v388.0.0.23.106/messenger-v388.0.0.23.106.apk)
+
+[See all versions](https://github.com/Eltion/Messenger-SSL-Pinning-Bypass/releases/)
+
+## Run using Frida (Requires Root)
+
+This method requires frida-tools and also frida-server running in the device
+```
+frida -U -l .\messenger-ssl-pinning-bypass.js -f com.facebook.orca --no-pause
+```
+
+## Patch APK
+
+You can create your own patched APK. 
+
+
+### Requirements Linux (Ubuntu):
+1. Install java JRE: `sudo apt install default-jre`
+2. Install apksigner: `sudo apt install apksigner`
+3. Install zipalign: `sudo apt install zipalign`  
+
+Note: apksigner and zipalign can also be found in android sdk [build-tools](https://dl.google.com/android/repository/build-tools_r30.0.1-linux.zip)
+
+### Requirements Windows:
+1. Install java JRE
+2. Download [build-tools](https://dl.google.com/android/repository/build-tools_r30.0.1-windows.zip) and unzip
+3. Add unzip folder to path variable
+
+### Instructions
+
+1. Download Messenger apk file.
+2. Install requirements > `pip install -r requirements.txt`
+3. Run script > `python patch_apk.py -i <input apk> -o <output apk>`
+
+After that an patched apk file should be generated.
+
+## Intercept network traffic
+
+You can use a tool like mitmproxy or Burp Suite to intercept the network.
+
+1. Install patched APK in the device
+2. Install [mitmproxy](https://mitmproxy.org/) or [Burp Suite](https://portswigger.net/burp)
+3. Set up proxy for wifi settings or run: `adb shell settings put global http_proxy <proxy>`
+
+Now you should be able to see the network traffic.
+
+![image](https://user-images.githubusercontent.com/18504798/204705212-7affe211-164e-4d9e-b573-443dff68c4ca.png)
+
+## View script logs
+To view the logcat run:
+```
+adb logcat -s "MESSENGER_SSL_PINNING_BYPASS:V"
+```
+
+[#leftenter](#leftenter)
---
+++ messenger-ssl-pinning-bypass.js
@@ -0,0 +1,101 @@
+function patch(library) {
+	let found = false;
+	const pattern = "b5 01 00 b4 80 82 4c 39";
+	Memory.scan(library.base, library.size, pattern, {
+		onMatch(address, size) {
+			Memory.patchCode(address, 4, code => {
+				found = true;
+				const cw = new Arm64Writer(code);
+				cw.putBytes([0xb5, 0x01, 0x0, 0xb5]);
+                cw.flush();
+				return 'stop';
+			});
+			logger(`[*][+] Patched libcoldstart.so`);
+		},
+		onComplete() {
+			if (!found) {
+				logger(`[*][-] Failed to find pattern: ${pattern}`);
+			}
+		}
+	});
+
+}
+
+function logger(message) {
+	console.log(message);
+	Java.perform(function () {
+		var Log = Java.use("android.util.Log");
+		Log.v("MESSENGER_SSL_PINNING_BYPASS", message);
+	});
+}
+
+function waitForModule(moduleName) {
+	return new Promise(resolve => {
+		const interval = setInterval(() => {
+			const module = Process.findModuleByName(moduleName);
+			if (module != null) {
+				clearInterval(interval);
+				resolve(module);
+			}
+		}, 10);
+	});
+}
+
+
+Java.perform(function () {
+    try {
+        var array_list = Java.use("java.util.ArrayList");
+        var ApiClient = Java.use('com.android.org.conscrypt.TrustManagerImpl');
+        if (ApiClient.checkTrustedRecursive) {
+            logger("[*][+] Hooked checkTrustedRecursive")
+            ApiClient.checkTrustedRecursive.implementation = function (a1, a2, a3, a4, a5, a6) {
+                var k = array_list.$new();
+                return k;
+            }
+        } else {
+            logger("[*][-] checkTrustedRecursive not Found")
+        }
+    } catch (e) {
+        logger("[*][-] Failed to hook checkTrustedRecursive")
+    }
+});
+
+Java.perform(function () {
+    try {
+        const x509TrustManager = Java.use("javax.net.ssl.X509TrustManager");
+        const sSLContext = Java.use("javax.net.ssl.SSLContext");
+        const TrustManager = Java.registerClass({
+            implements: [x509TrustManager],
+            methods: {
+                checkClientTrusted(chain, authType) {
+                },
+                checkServerTrusted(chain, authType) {
+                },
+                getAcceptedIssuers() {
+                    return [];
+                },
+            },
+            name: "com.leftenter.messenger",
+        });
+        const TrustManagers = [TrustManager.$new()];
+        const SSLContextInit = sSLContext.init.overload(
+            "[Ljavax.net.ssl.KeyManager;", "[Ljavax.net.ssl.TrustManager;", "java.security.SecureRandom");
+        SSLContextInit.implementation = function (keyManager, trustManager, secureRandom) {
+            SSLContextInit.call(this, keyManager, TrustManagers, secureRandom);
+        };
+        logger("[*][+] Hooked SSLContextInit")
+    } catch (e) {
+        logger("[*][-] Failed to hook SSLContextInit")
+    }
+});
+
+
+waitForModule("libcoldstart.so").then(lib => {
+	patch(lib);
+});
+
+
+waitForModule("libgadget.so").then(lib => {
+	console.log("libgadget.so")
+});
+
